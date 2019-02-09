e32c2a91fa357f88b392b87efb2bdd0167c991db
Charlie Root <root@cibert.hq.c3d2.de>
2019-02-10 00:12:55 +0100

    fix

--- log.sh
+++ log.sh
@@ -2,7 +2,7 @@ LOGDIR=/home/kellerlog
 
 mkdir -p $LOGDIR
 
-cu -l /dev/cuaU0 -s 9600 | \
+cu -l /dev/cuaU0 -s 9600 2>/dev/null < /dev/zero | \
 	awk '{cmd="(date +%s)"; cmd | getline d; print d, $0; close(cmd)}' \
 	>> "$LOGDIR/$(date +%Y-%m-%d)"
 
--- run-log.sh
+++ run-log.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cd $(dirname $0)
+
 (while true; do
 	sh log.sh
+	sleep 1
 done) &
